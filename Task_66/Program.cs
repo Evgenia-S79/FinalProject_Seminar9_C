e3aa2805c78d2f3e9031c4772cde611888452ff0
Evgenia S <Dla-evgenii@yandex.ru>
2022-11-01 21:01:20 +0600

    Задача 66 (Сумма натуральных эле ментов в промежутке от M до N)

---
+++ Task_66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+
+int GetNumberFromConsole(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumRecursion(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + SumRecursion(m, n - 1);
+}
+
+int m = GetNumberFromConsole("Введите первое значение (m)");
+int n = GetNumberFromConsole("Введите второе значение (n)");
+
+Console.WriteLine($"Сумма элементов от {m} до {n} = {SumRecursion(m, n)}");
+
