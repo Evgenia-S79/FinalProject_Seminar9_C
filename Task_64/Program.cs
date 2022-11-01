41e23731ae9891033de7ce1623c0beb302217a13
Evgenia S <Dla-evgenii@yandex.ru>
2022-11-01 21:00:01 +0600

    Задача 64 (Выведение натуральных чисел от N до 1. Рекурсия)

---
+++ Task_64/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные 
+// числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+double GetNumberFromConsole()
+{
+    Console.Write("Введите число: ");
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+void PrintNumberRecursion(double number, int counter)
+{
+    if (counter > number) return;
+    PrintNumberRecursion(number, counter + 1);
+        if (counter != 1)
+           Console.Write(counter + ", ");
+        else
+            Console.Write(counter + ".");
+}
+
+double number = GetNumberFromConsole();
+int counter = 1;
+PrintNumberRecursion(number, counter);
